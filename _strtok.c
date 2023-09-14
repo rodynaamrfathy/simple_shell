1d6cc6463b37c81de9788b083cf86505d22ac558
rodynaamrfathy <rodynaamr@icloud.com>
2023-09-14 07:18:38 -0700

    ‘hi’

--- _strtok.c
+++ _strtok.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * _strtok - 
+ * _strtok - function involves parsing a string and splitting it into tokens
+ * based on a delimiter character.
  *
- *
+ * Return: token
  */
 char *strtok(char *str, const char *delim)
 {
-
+	
 }
