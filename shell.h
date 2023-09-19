ba75f9c5824d8a64690e369b7aa55f106631c1bc
rodynaamrfathy <rodynaamr@icloud.com>
2023-09-19 15:31:23 -0700

    ‘version’

--- shell.h
+++ shell.h
@@ -22,12 +22,12 @@ int _snprintf(char *str, size_t size, char *str1, char *str2);
 int _putchar(char c);
 void _sprintf(char *str);
 char *_strtok(char *str, const char *delimiters);
-int _strcmp(char *str1,char *str2);
+int _strcmp(char *str1, char *str2);
 
 /* functions to handle special commands */
 void _argv(char ***argv, char *line);
 void print_environment(char **environ);
-void cdd(char *argv[], char previous_directory[], size_t previous_directory_size);
+void cdd(char *argv[], char previous_directory[], size_t prev_directory_size);
 
 /* built-in functions */
 void _shell(char **argv);
